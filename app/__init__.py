106648238cced5c5aa02579f7243ca0fc2a49d55
ElderMee <981258307@qq.com>
2022-08-11 17:34:08 +0800

    day1 finished
    完成了项目框架，模板使用，表单，数据库

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask  # 从flask包中导入Flask类
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
 
 app = Flask(__name__)  # 将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
 
 # print('等会谁（哪个包或模块）在使用我：', __name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # 放在后面可以防止循环引用
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,31 @@
+from flask import render_template, redirect, flash, url_for
+
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'hello world!'
+    user = {'username': 'eldermee'}
+    posts = [  # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
+    # return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/Login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()  # 表单实例化对象
+    if form.validate_on_submit():
+        flash('Login requested for user {},remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
